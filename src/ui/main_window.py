ac1bde8df7d2e4094cd74d5b3c9cb3cf5cac0572
cocoknightcom <contact@cocoknight.com>
2024-11-05 22:28:38 +0100

    loading wip

--- src/ui/main_window.py
+++ src/ui/main_window.py
@@ -14,30 +14,7 @@ class MainWindow(Gtk.Window):
         self.stack_switcher = Gtk.StackSwitcher()
         self.stack_switcher.set_stack(self.stack)
         
-        # Initialize views (this will be done after we show the loading spinner)
-        self.home_view = None
-        self.package_detail_view = None
-        self.log_view = None
-        self.settings_view = None
-
-        # Layout
-        vbox = Gtk.VBox()
-        vbox.pack_start(self.stack_switcher, False, False, 0)
-        vbox.pack_start(self.stack, True, True, 0)
-        self.add(vbox)
-
-        # Show the loading indicator
-        self.loading_spinner = Gtk.Spinner()
-        self.loading_spinner.set_margin_top(20)
-        vbox.pack_start(self.loading_spinner, True, False, 0)
-        self.loading_spinner.start()  # Start the spinner
-        self.show_all()  # Display the window first
-
-        # Load views in the background
-        GLib.idle_add(self.load_views)
-
-    def load_views(self):
-        # Now initialize views
+        # Initialize views
         self.home_view = HomeView(self)
         self.package_detail_view = PackageDetailView(self)
         self.log_view = LogView(self)
@@ -49,11 +26,15 @@ class MainWindow(Gtk.Window):
         self.stack.add_titled(self.log_view, "log", "Logs")
         self.stack.add_titled(self.settings_view, "settings", "Settings")
 
-        # Stop the loading spinner
-        self.loading_spinner.stop()
-        self.remove(self.loading_spinner)  # Remove the spinner from the window
-        return False  # Stop calling this function
+        # Layout
+        vbox = Gtk.VBox()
+        vbox.pack_start(self.stack_switcher, False, False, 0)
+        vbox.pack_start(self.stack, True, True, 0)
+        self.add(vbox)
 
+        # Show the main window immediately
+        self.show_all()  # Display the window first
+        
     def navigate_to(self, view_name):
         if view_name == "home":
             self.stack.set_visible_child(self.home_view)
@@ -63,4 +44,3 @@ class MainWindow(Gtk.Window):
             self.stack.set_visible_child(self.log_view)
         elif view_name == "settings":
             self.stack.set_visible_child(self.settings_view)
-
